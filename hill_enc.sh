47f36ab610e66b244f777997a750b9f42cc1d416
Azfar Alsukor <azfaralsukor@gmail.com>
2017-01-12 11:27:42 +0800

    made translator into a function for reusability

--- hill_enc.sh
+++ hill_enc.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+translator(){
+	input=`echo $1 | tr '[:lower:]' '[:upper:]'`
+	letter=$((`echo ${input}|od -t d1|awk '{printf "%s",$2}';`-65))	
+	return $letter
+}
 echo -n "Enter a letter: "
 read input
-input=`echo $input | tr '[:lower:]' '[:upper:]'`
-letter=`echo ${input}|od -t d1|awk '{printf "%s",$2}';`
-echo $(($letter-65))
+translator $input
+letter=$?
+echo $letter
